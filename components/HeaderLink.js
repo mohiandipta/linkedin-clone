bf3475b558f38de1e2f4fccef3c009bc984b982a
mohiandipta <mohianmostofa@gmail.com>
2023-01-07 21:40:05 +0600

    modified HeaderLink

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const HeaderLink = ({Icon, text}) => {
-    return (
-        <div>
-            <div>
-                <Icon className="!h-7 !w-7 lg:!-b-1"/>
-            </div>
-            {text}
-        </div>
-    );
-}
+const HeaderLink = ({ Icon, text, avatar, feed }) => {
+  return (
+    <div
+      className={`cursor-pointer flex flex-col justify-center items-center 
+        ${
+          feed
+            ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1"
+            : "feed-false"
+        }`}
+    >
+      {avatar ? <Icon className="!h-7 !w-7 lg:!-b-1" /> : <Icon />}
+      <h4 className="text-sm">{text}</h4>
+    </div>
+  );
+};
 
 export default HeaderLink;
